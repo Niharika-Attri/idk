b687f76dd7049b4ecfe470952f85d24cfaeadbbf
Niharika_Attri <niharikaattri2005@gmail.com>
2025-05-01 22:33:10 +0530

    corrected fonts usage

--- idk/src/App.jsx
+++ idk/src/App.jsx
@@ -1,9 +1,7 @@
 import Homepage from './pages/homepage'
 function App() {
   return (
-    <div className='flex w-screen h-screen bg-gray-900'>
-      <Homepage/>
-    </div>
+    <Homepage/>
   )
 }
 
